63a2383b0d7ceeae546dfd39f213ac7df665e352
Sandy <cakl2313@gmail.com>
2023-05-31 17:58:34 +0800

    Use the user ID as the key and store the value in JSON format

--- redisServer.py
+++ redisServer.py
@@ -1,3 +1,5 @@
+import json
+
 import redis
 import os
 
@@ -5,11 +7,22 @@ redis_server = redis.from_url(os.environ.get("REDIS_URL"))
 env_table = 'env_table'
 
 
-def setEnvVariable(message):
+def setEnvVariable(user_id, message):
+
     message_list = message.split(' ')
     envKey = str(message_list[1])
     envValue = str(message_list[2])
 
-    redis_server.hset(env_table, envKey, envValue)
+    db_value = redis_server.hget(env_table, user_id)
+
+    if db_value is None:
+        value_json = {envKey: envValue}
+        json_string = json.dumps(value_json)
+    else:
+        db_json = json.loads(db_value)
+        db_json[envKey] = envValue
+        json_string = json.dumps(db_json)
+
+    redis_server.hset(env_table, user_id, json_string)
 
     return "done"
